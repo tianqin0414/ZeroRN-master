bcdb94c163fa2e5b6160296aa81d33f46f00699d
ZeroIos <387956977@qq.com>
2018-09-01 20:41:00 +0800

    0901

--- src/commons/utils/index.js
+++ src/commons/utils/index.js
@@ -7,10 +7,10 @@
  * 定义全局变量, 全局属性, 全局函数, 全局引用等
  */
 
-declare var px2dp: any;
+// declare var px2dp: any;
 declare var FONT_SIZE: any;
 declare var SCREEN_WIDTH: number;
-declare var SCREEN_HEIGHT: number;
+// declare var SCREEN_HEIGHT: number;
 declare var iOS: boolean;
 declare var Android: boolean;
 declare var Toast: Function;
--- src/containers/initialTQ/index.js
+++ src/containers/initialTQ/index.js
@@ -1,42 +1,11 @@
 import React, { Component, } from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
-import { global, } from '../../commons/utils';
+import { global, } from '../../commons/utils/Global';
 
 
 // 橘色 #F94301
 
-const Dimensions = require('Dimensions');
 
-const h = px2dp(80);
-// let BHeight= 50;
-const styles = StyleSheet.create({
-  wrapper: {
-    marginHorizontal: 10,
-    marginVertical: Dimensions.get('window').height - 80,
-    backgroundColor: '#0e0204',
-    height: 80,
-    // borderRadius: 7,
-	  flexDirection: 'row',
-	  // alignItems:'stretch',
-    justifyContent: 'space-around',
-  },
-
-  right: {
-	  marginLeft: 0,
-	  borderRadius: 7,
-	  height: 50,
-	  backgroundColor: '#F94301',
-  },
-  left: {
-    fontSize: 20,
-    marginRight: 10,
-  },
-
-  textRight: {
-	  fontSize: 20,
-    color: '#f3f6f9',
-  },
-});
 export default class Rule extends Component {
  static navigationOptions = {
    header: null,
@@ -55,35 +24,80 @@ export default class Rule extends Component {
 
  render() {
    return (
-     <View style={styles.wrapper}>
+     <View style={styles.entire}>
+       <View style={styles.wrapper}>
 
-       <View
-         style={styles.right}
-         onPress={() => {
+         <View
+           style={styles.button}
+           backgroundColor={'#F94301'}
+           onPress={() => {
 		     this.goLogin();
 	     }}
-       >
-         <Text style={styles.textRight}>登入</Text>
-
-       </View>
-       <View
-         style={styles.right}
-         onPress={() => {
+         >
+           <Text
+             style={[ styles.buttonFont, { color: 'white', }, ]}
+           >
+		         登录
+	         </Text>
+         </View>
+         <View
+           style={styles.button}
+           backgroundColor={'#F8F8F8'}
+           onPress={() => {
 			     this.goLogin();
 		     }}
-       >
-         <Text style={styles.textRight}>注册</Text>
+         >
+           <Text
+             style={[ styles.buttonFont, { color: 'black', }, ]}
+           >
+             注册
+           </Text>
 
+         </View>
        </View>
-       {/* <View */}
-       {/* style={styles.left} */}
-       {/* onPress={() => { */}
-       {/* this.goRegister(); */}
-       {/* }} */}
-       {/* > */}
-       {/* 注册 */}
-       {/* </View> */}
      </View>
    );
  }
 }
+
+const FH = px2dp(180);
+const CH = px2dp(100);
+const CW = px2dp(250);
+// let BHeight= 50;
+const styles = StyleSheet.create({
+  entire: {
+    backgroundColor: '#aeff93',
+  },
+
+  wrapper: {
+    marginHorizontal: px2dp(40),
+    marginVertical: SCREEN_HEIGHT - FH,
+    // backgroundColor: '#0e0204',
+    height: FH,
+    // borderRadius: 7,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  button: {
+    borderRadius: 7,
+	  height: CH,
+	  width: CW,
+
+    alignItems: 'center',
+	  justifyContent: 'center',
+  },
+
+  right: {
+    borderRadius: 7,
+	  width: CW,
+    height: CH,
+    backgroundColor: '#F94301',
+  },
+
+
+  buttonFont: {
+    fontSize: FONT_SIZE(20),
+    // color: '#f3f6f9',
+  },
+});
